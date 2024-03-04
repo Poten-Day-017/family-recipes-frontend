950039ab790a1e395dde8aa8ae62b62c3315ec79
nyoung113 <mysoulk0113@gmail.com>
2024-03-04 09:22:03 +0900

    feat: 레시피 생성 페이지에 사용할validation 라이브러리 설치 및 일부 적용

---
+++ src/constants/category.ts
@@ -0,0 +1,30 @@
+const CATEGORY_LIST = [
+  {
+    name: "한식",
+    code: "001",
+  },
+  {
+    name: "중식",
+    code: "002",
+  },
+  {
+    name: "양식",
+    code: "003",
+  },
+  {
+    name: "일식",
+    code: "004",
+  },
+  {
+    name: "분식",
+    code: "005",
+  },
+  {
+    name: "디저트",
+    code: "006",
+  },
+  {
+    name: "기타",
+    code: "999",
+  },
+] as const;
